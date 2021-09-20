fdbba03574e98010af1d2e95cf816c9f6a746c46
Carlos Franco <me@cfrco.com>
2021-09-19 23:44:58 -0700

    final commit

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://www.davidchuschinabistro.com')
+.constant('ApiPath', 'https://morning-cove-81665.herokuapp.com/')
 .config(config);
 
 config.$inject = ['$httpProvider'];
